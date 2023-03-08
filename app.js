4f0a047ae7c48e1e87f4c5011b8357d669ed562c
gary909 <garywhite61@hotmail.com>
2023-03-08 12:53:06 +0100

    code working

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-//return price without vat
 function excludingVatPrice(price){
-    // your code
-    return 0;
-}
+    let noVat = Number(((price / 115) * 100).toFixed(2));
+      if (price == null){
+          return -1;
+      } else {
+          return noVat;
+      }
+  }
 
-console.log(230); // 200.00
-console.log(123); // 106.96
+console.log(excludingVatPrice(230)); // 200.00
+console.log(excludingVatPrice(123)); // 106.96
+console.log(excludingVatPrice(115)); // 100.00
